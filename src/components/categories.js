f0e522a415de99f3ef05b0c1351f4efcb1c626c2
wolterbwambale <benplaan1@gmail.com>
2023-06-25 00:36:48 +0300

    fix required changes

--- src/components/categories.js
+++ src/components/categories.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 function Categories() {
   return (
-    <div>
-      <h1>This is categories sections</h1>
-    </div>
+    <div className="cat-h"><h1>This is categories sections</h1></div>
   );
 }
 
